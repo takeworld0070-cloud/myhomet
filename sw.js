b6bf641869df33e6edb879c183f2f4661f7865dd
takeworld0070-cloud <takeworld0070@gmail.com>
2026-02-17 19:26:43 +0900

    v2
    v2

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'homet-v2-cache';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
